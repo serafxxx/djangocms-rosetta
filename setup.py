1c07d633b7f1f3a521960600e0a950e963eebbb2
Philipp Zedler <philipp@neue-musik.com>
2015-09-30 18:35:57 +0200

    A setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+setup(
+    
+    name = 'djangocms-rosetta',
+    
+    version = '0.0',
+    
+    description = 'Link to the rosetta interface in the Django CMS Toolbar',
+    
+    url = 'https://github.com/philippze/djangocms-rosetta',
+    
+    author = 'Philipp Zedler',
+    author_email = 'philipp@neue-musik.com',
+    
+    license = 'MIT',
+    
+    keywords = 'Django translation rosetta',
+    
+    install_requires=['django', 'django-cms', 'rosetta']
+    
+)
